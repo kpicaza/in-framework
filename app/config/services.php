566ff6b6e2a26283d2c308c50c45eb9131915173
kpicaza <kpicaza.sound@gmail.com>
2016-08-14 15:01:10 +0200

    Add scrutinizer CI & Create empty README.md

--- app/config/services.php
+++ app/config/services.php
@@ -1,15 +1,5 @@
 <?php
 
-use Symfony\Component\Validator\Mapping\Factory\LazyLoadingMetadataFactory;
-use Symfony\Component\Validator\Mapping\Loader\YamlFileLoader;
-
-use InCRM\Persistence\Doctrine\Repository\OrganizationRepository;
-use InCRM\App\Controller\OrganizationController;
-use InCRM\Domain\Factory\OrganizationFactory;
-use InCRM\Manager\OrganizationManager;
-use InCRM\Form\Type\OrganizationFormType;
-use InCRM\Form\Handler\OrganizationHandler;
-
 //$app['validator.mapping.class_metadata_factory'] = new LazyLoadingMetadataFactory(
 //    new YamlFileLoader(
 //        __DIR__ . '/../../core/Form/Validation/Organization.yml'
